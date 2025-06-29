d0c89322872d2b92a9f1aa53c787941677669564
Roman Salmin <Roman.Salmin@gmail.com>
2025-06-29 18:41:43 +0300

    fixed version and add clang-tools for covinience

--- flake.nix
+++ flake.nix
@@ -13,11 +13,12 @@
       in
         pkgs.stdenv.mkDerivation {
           pname = "dwl";
-          version = "0.7+";
+          version = "0.8-dev+";
           src = self;
 
           nativeBuildInputs = with pkgs; [
             pkg-config
+            clang-tools # for lsp for emacs
           ];
 
           buildInputs = with pkgs; [
